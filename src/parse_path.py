4e483778043034c8551d8fb32a605b4fdb044444
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-07-03 19:20:52 +0100

    use with and iterate file rather than readlines()

--- src/parse_path.py
+++ src/parse_path.py
@@ -14,50 +14,55 @@ NOTE:
 def only_tifs(x):
     """Returns a list of filenames that are .tifs"""
     images = []
-    for line in open(x, "r").readlines():
-        if ".tif" in line:
-            images.append(str(line))
+    with open(x) as f:
+        for line in f:
+            if ".tif" in line:
+                images.append(str(line))
     return images
 
 def get_filename(x):
     """ Get the final image URL"""
     files = []
-    for line in open(x, "r").readlines():
-        filename = str(line.split("/")[-1])
-        files.append(filename)
+    with open(x) as f:
+        for line in f:
+            filename = str(line.split("/")[-1])
+            files.append(filename)
     return files
 
 def get_path(x):
     """Get the path up until the image URL"""
     paths = []
-    for line in open(x, "r").readlines():
-        p, f = os.path.split(line)
-        paths.append(p)
+    with open(x) as f:
+        for line in f:
+            p, f = os.path.split(line)
+            paths.append(p)
     return paths
 
 
 def get_platenum(x):
     """ Get the platenumber"""
     plates = []
-    for line in open(x).readlines():
-        num = int(line.split("/")[-2])
-        plates.append(num)
+    with open(x) as f:
+        for line in f:
+            plates.append(int(line.split("/")[-2]))
     return plates
 
 
 def get_platename(x):
     """Get the user-defined plate name"""
     plates = []
-    for line in open(x).readlines():
-        plates.append(line.split("/")[-4])
+    with open(x) as f:
+        for line in f:
+            plates.append(line.split("/")[-4])
     return plates
 
 
 def get_date(x):
     """Get the date of imaging for a plate"""
     dates = []
-    for line in open(x).readlines():
-        dates.append(line.split("/")[-3])
+    with open(x) as f:
+        for line in f:
+            dates.append(line.split("/")[-3])
     return dates
 
 
