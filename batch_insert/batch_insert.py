161c3bce12c2eacd2c2a4722f65f664c69c72f51
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-07-06 18:53:30 +0100

    for inserting CP commands into qsub

---
+++ batch_insert/batch_insert.py
@@ -0,0 +1,28 @@
+#! /bin/env python
+
+def write_batch_scripts(template, placeholder, batch_file):
+
+    """
+    converts a template qub submission script and a batch output file produced
+    by cellprofiler to multiple consecutively numbered submission scripts
+    arguments:
+    ----------
+    template   -- template submission script for qsub
+    placehoder -- placeholder string within the template file. This will be
+                  substituted with the cellprofiler command
+    batch_file -- file produced by cellprofiler --get-batch-commands
+    """
+
+    tmp = open(template).read()
+    inputs = open(batch_file).readlines()
+
+    for i, cmd in enumerate(inputs):
+        out = tmp.replace(placeholder, cmd)
+        outfile = open("out_{}".format(i), "w")
+        outfile.write(out)
+
+
+if __name__ == "__main__":
+    write_batch_scripts(template="test_cp_batch_run.sh",
+                        placeholder="PLACEHOLDER",
+batch_file="batch_out.txt")
