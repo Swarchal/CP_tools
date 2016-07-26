f041ad9a7dec570ab35d242ea569981a8d354d65
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-07-26 18:45:12 +0100

    better handling of different index types
    Now works with both multi-indexed and typical DataFrames by specifying header rows

--- merge_ouput/merge_to_db.py
+++ merge_ouput/merge_to_db.py
@@ -3,6 +3,7 @@ import pandas as pd
 import colfuncs
 from sqlalchemy import create_engine
 
+#TODO enable multiple tables for each datatype, i.e table for DATA, IMAGE
 
 class ResultsDirectory:
 
@@ -12,20 +13,18 @@ class ResultsDirectory:
     - create_db(): creates an sqlite database in the results directory
     - to_db(): loads the csv files in the directory and writes them as
                tables to the sqlite database created by create_db()
-    - truncate: argument will name files from text between the last underscore
-      and .csv. e.g 'date_plate_cell.csv' will return 'cell'
     """
 
     def __init__(self, directory):
-        """Get full filepaths of all files in a directory, including sub-directories"""
+        """
+        Get full filepaths of all files in a directory, including sub-directories
+        """
         file_paths = []
 
         for root, directories, files in os.walk(directory):
             for filename in files:
                 filepath = os.path.join(root, filename)
-                # currently only getting DATA.csv files
-                if filename.endswith("DATA.csv"):
-                    file_paths.append(filepath)
+                file_paths.append(filepath)
             self.file_paths = file_paths
 
 
@@ -36,18 +35,29 @@ class ResultsDirectory:
 
     # write csv files to database
     # currently appending all to the same table
-    def to_db(self):
-        for x, _ in enumerate(self.file_paths):
-            f = self.file_paths[x]
-            tmp_file = pd.read_csv(f, header=[0,1], iterator=True,
-                chunksize=1000)
-            all_file = pd.concat(tmp_file)
-            all_file.columns = colfuncs.collapse_cols(all_file)
-            all_file.to_sql("DATA", con=self.engine,
-                flavor ="sqlite", index=False, if_exists="append",
-                chunksize=1000)
+    def to_db(self, select="DATA", header=[0,1]):
+        """
+        select: the name of the .csv file, this will also be the database table
+        header: the number of header rows.
+                N.B. if not multi-indexed then use 0 rather than [0].
+        """
+        # filter files
+        file_paths = [f for f in self.file_paths if f.endswith(select+".csv")]
+
+        for x, _ in enumerate(file_paths):
+            f = file_paths[x]
+            tmp_file = pd.read_csv(f, header=header, iterator=True)
+            all_file = pd.concat(tmp_file) # collect iterator
+
+            # collapse column names if multi-indexed
+            if isinstance(all_file.columns, pd.core.index.MultiIndex):
+                all_file.columns = colfuncs.collapse_cols(all_file)
+
+            all_file.to_sql(select, con=self.engine,
+                flavor ="sqlite", index=False, if_exists="append")
 
 if __name__ == '__main__':
     x = ResultsDirectory("/home/scott/multi_index_test")
     x.create_db("/home/scott")
-    x.to_db()
+    x.to_db(select="DATA", header=[0,1])
+    x.to_db(select="IMAGE", header=0)
