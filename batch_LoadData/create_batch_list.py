81f2c224d027d8c42f63681a1740c54b6ade6e86
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-08-31 17:22:49 +0100

    fix indent error

--- batch_LoadData/create_batch_list.py
+++ batch_LoadData/create_batch_list.py
@@ -27,23 +27,17 @@ def create_batch_list(file_list, pipeline, n_chunks=None,
         n_chunks - number of processes to run in parallel
         output_prefix - prefix for the output folder
     """
-
     n_imagesets = count_lines(file_list)
-
     if n_chunks != None and chunk_size != 20:
         print ValueError("Cannot specify both chunk_size and n_chunks")
-
     if n_chunks == None or 0:
         #split into chunks containing roughly 20 imagesets
         n_chunks = n_imagesets // chunk_size
-
     chunk_size = n_imagesets // n_chunks
     remainder = n_imagesets - (chunk_size * n_chunks)
-
     if full_path == False:
         # trim path to just file name
         file_list = file_list.split(os.sep)[-1]
-
     output = []
     for i, val in enumerate(range(0, n_chunks), 1):
         if i < n_chunks:
@@ -65,14 +59,11 @@ def create_batch_list(file_list, pipeline, n_chunks=None,
                 i
             )
         output.append(x)
-
     assert len(output) == n_chunks
-
     return output
 
 
 if __name__ == '__main__':
-
     # quick check
     out =  create_batch_list(
         file_list="/home/scott/Dropbox/CP_tools/make_LoadData/load_data_input.csv",
@@ -81,6 +72,6 @@ if __name__ == '__main__':
         output_prefix="/exports/eddie/scratch/s1027820/output_",
         full_path=False)
 
-with open("out_file.txt", "w") as f:
-    for i in out:
-        f.write(i + "\n")
+    with open("out_file.txt", "w") as f:
+        for i in out:
+            f.write(i + "\n")
