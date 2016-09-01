42f5ed25ee58bed745e2c6d1684eb854a728f93c
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-09-01 09:58:07 +0100

    tidy-up

--- make_LoadData/filenames.sh
+++ make_LoadData/filenames.sh
@@ -7,9 +7,6 @@ if [ $# -eq 0 ]
         exit 1
 fi
 
-
-
-
 # create list of filenames and store in a temporary file
 echo " |█    | Creating file-list"
 # check the directory exists
@@ -17,48 +14,39 @@ if [ ! -d "$1" ]
     then
         echo "ERROR: $1 is not a valid directory"
         exit 1
+    else
+        file="$1"
+        find "$file" -type f | grep -v "thumb\|.db" > tmp/filenames.txt
 fi
-file="$1"
-find "$file" -type f | grep -v "thumb\|.db" > tmp/filenames.txt
-
-
 
 # parse file names to extract metadata columns, save as csv
 echo " |██   | Extracting metadata from filenames"
 python src/create_loadData.py
-
-
-
 
 # reshape .csv file so that we have a column per channel
 # and rename columns for CellProfiler
 echo " |███  | Reshaping csv file"
 Rscript src/reshape.R
 
-
-
-
 echo " |████ | Merging any external metadata"
 if [ ! -z "$2" ];
+    # if there is a second argument
     then
-
-        # check the external metadata file exists
-        # if not, then clean-up and exit
         if [ ! -f "$2" ]
+            # check the external metadata file exists
             then
+                # if not, then clean-up and exit
                 echo "ERROR: $2 is not a valid file"
                 rm tmp/filenames.txt
                 exit 1
+            else
+                # if file is good, then pass to python script
+                echo "  - Metadata argument passed"
+                python src/add_metadata.py "$2"
         fi
-
-        echo "   - Metadata argument passed"
-        python src/add_metadata.py metadata_file "$2"
 fi
-
-
-
-
 
 echo " |█████| Removing temporary files"
 rm tmp/filenames.txt
 rm tmp/py_load_long.csv
+exit 0
