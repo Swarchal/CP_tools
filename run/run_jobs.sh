5275a851af54dd950df071eb3a26bc2a414f0992
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-09-22 18:39:39 +0100

    change to directory to submit jobs

--- run/run_jobs.sh
+++ run/run_jobs.sh
@@ -4,15 +4,19 @@
 # argv is the directory containing all SGE submission scripts create by
 # batch_insert
 
+
+# check inputs
 if [ $# -eq 0 ]; then
     echo "ERROR: No arguments supplied"
     exit 1
 fi
-
 if [ ! -d "$1" ]; then
     echo "ERROR: $1 is not a valid directory"
     exit 1
 fi
+
+# move to directory containing submission scripts
+cd "$1"
 
 # create file listing all submission scripts
 ls "$1" > .jobs_to_run.txt
@@ -22,6 +26,7 @@ while read job; do
     qsub $job
 done < jobs_to_run.txt
 
+# tidy up afterwards
 rm .jobs_to_run.txt
 
 exit 0
