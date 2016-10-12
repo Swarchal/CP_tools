19e61cca6b7e42fedf5051fc80bfa5d2b4c36c3f
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-10-12 14:49:29 +0100

    aggregate keep metadata, fixes #6

--- 5_merge_ouput/merge_to_db.py
+++ 5_merge_ouput/merge_to_db.py
@@ -5,11 +5,6 @@ import numpy as np
 import colfuncs
 from sqlalchemy import create_engine
 
-def _check_agg_func(agg_func):
-    """check if agg_func is a valid function"""
-    valid_agg_funcs = ["median", "mean"]
-    if agg_func not in valid_agg_funcs:
-        ValueError("Invalid agg_func, options are: 'median', 'mean'")
 
 class ResultsDirectory:
 
@@ -30,7 +25,6 @@ class ResultsDirectory:
         Get full filepaths of all files in a directory, including sub-directories
         """
         file_paths = []
-
         for root, directories, files in os.walk(directory):
             for filename in files:
                 filepath = os.path.join(root, filename)
@@ -55,25 +49,23 @@ class ResultsDirectory:
         """
         # filter files
         file_paths = [f for f in self.file_paths if f.endswith(select+".csv")]
-
         # check there are files matching select argument
         if len(file_paths) == 0:
             raise ValueError("No files found matching '{}'".format(select))
-
         for x in file_paths:
-            if header == 0:
+            if header == 0 or header == [0]:
                 # dont need to collapse headers
                 print("importing :", x)
                 tmp_file = pd.read_csv(x, header=header, chunksize=10000,
-                        iterator=True, **kwargs)
+                                       iterator=True, **kwargs)
                 all_file = pd.concat(tmp_file)
                 all_file.to_sql(select, con=self.engine, flavor="sqlite",
-                        index=False, if_exists="append")
+                                index=False, if_exists="append")
             else:
                 # have to collapse columns, means reading into pandas
                 print("importing :", x)
                 tmp_file = pd.read_csv(x, header=header, chunksize=10000,
-                    iterator=True, **kwargs)
+                                       iterator=True, **kwargs)
                 all_file = pd.concat(tmp_file)
                 # collapse column names if multi-indexed
                 if isinstance(all_file.columns, pd.core.index.MultiIndex):
@@ -81,39 +73,30 @@ class ResultsDirectory:
                 else:
                     TypeError("Multiple headers selected, yet dataframe is not multi-indexed")
                 all_file.to_sql(select, con=self.engine,
-                    flavor="sqlite", index=False, if_exists="append")
+                                flavor="sqlite", index=False,
+                                if_exists="append")
 
 
     def to_db_agg(self, select="DATA", header=0, by="ImageNumber",
-                  agg_func="median", **kwargs):
+                  method="median", **kwargs):
         """
         select - string, the name of the .csv file, this will also be the
                  prefix of the database table name.
         header - int or list, the number of header rows.
                  N.B if not multi-indxed, then use 0 rather than [0].
         by - string, the column by which to group the data by.
-        **kwargs - additional arguments to pandas.read_csv
+        **kwargs - additional arguments to pandas.read_csv and aggregate
         """
-
-        # check agg_func is a valid function
-        _check_agg_func(agg_func)
-
         # filter files
         file_paths = [f for f in self.file_paths if f.endswith(select+".csv")]
-
         # check there are files matching select argument
         if len(file_paths) == 0:
             raise ValueError("No files found matching '{}'".format(select))
-
         for x in file_paths:
             if header == 0:
                 print("importing :", x)
                 tmp_file = pd.read_csv(x, header=header, **kwargs)
-                tmp_grouped = tmp_file.groupby(by, as_index=False)
-                if agg_func == "median":
-                    tmp_agg = tmp_grouped.aggregate(np.median)
-                elif agg_func == "mean":
-                    tmp_agg = tmp_grouped.aggregate(np.mean)
+                tmp_agg = _aggregate(tmp_file, on=by, method=method, **kwargs)
                 tmp_agg.to_sql(select+"_agg", con=self.engine, flavor="sqlite",
                                index=False, if_exists="append")
             else:
@@ -124,19 +107,139 @@ class ResultsDirectory:
                     tmp_file.columns = colfuncs.collapse_cols(tmp_file)
                 else:
                     TypeError("Multiple headers selected, yet dataframe is not multi-indexed")
-                tmp_grouped = tmp_file.groupby("Image_"+by, as_index=False)
-                if agg_func == "median":
-                    tmp_agg = tmp_grouped.aggregate(np.median)
-                elif agg_func == "mean":
-                    tmp_agg = tmp_grouped.aggregate(np.mean)
+                tmp_agg = _aggregate(tmp_file, on=by, method=method, **kwargs)
                 tmp_agg.to_sql(select+"_agg", con=self.engine, flavor="sqlite",
                                index=False, if_exists="append")
 
 
+def _aggregate(data, on, method="median", **kwargs):
+    """
+    Aggregate dataset
 
-if __name__ == '__main__':
-    x = ResultsDirectory("/mnt/datastore/scott/2016-07-13_SGC/raw_data")
-    x.create_db("/home/scott/2016-07-13_SGC/")
-    x.to_db_agg(select="DATA", header=[0,1])
-    x.to_db(select="DATA", header=[0,1])
-    x.to_db(select="IMAGE", header=[0,1])
+    Parameters
+    -----------
+    data : pandas DataFrame
+        DataFrame
+    on : string or list of strings
+        column(s) with which to group by and aggregate the dataset.
+    method : string (default="median")
+        method to average each group. options = "median" or "mean"
+    **kwargs : additional args to utils.get_metadata / utils.get_featuredata
+
+    Returns
+    -------
+    agg_df : pandas DataFrame
+        aggregated dataframe, with a row per value of 'on'
+    """
+    _check_inputs(data, on, method)
+    _check_featuredata(data, on, **kwargs)
+    # keep track of original column order
+    df_cols = data.columns.tolist()
+    grouped = data.groupby(on, as_index=False)
+    if method == "mean":
+        agg = grouped.aggregate(np.mean)
+    if method == "median":
+        agg = grouped.aggregate(np.median)
+    df_metadata = data[_get_metadata(data, **kwargs)].copy()
+    # add indexing column to metadata if not already present
+    df_metadata[on] = data[on]
+    # drop metadata to the same level as aggregated data
+    df_metadata.drop_duplicates(subset=on, inplace=True)
+    # merge aggregated and feature data
+    merged_df = pd.merge(agg, df_metadata, on=on, how="outer",
+                         suffixes=("remove_me", ""))
+    # merge untracked columns with merged data
+    merged_df = merged_df[df_cols]
+    # re-arrange to columns are in original order
+    assert len(merged_df.columns) == len(data.columns)
+    return merged_df
+
+
+def _check_inputs(data, on, method):
+    """ internal function for aggregate() to check validity of inputs """
+    valid_methods = ["median", "mean"]
+    if not isinstance(data, pd.DataFrame):
+        raise ValueError("not a a pandas DataFrame")
+    if method not in valid_methods:
+        raise ValueError("{} is not a valid method, options: median or mean".format(method))
+    df_columns = data.columns.tolist()
+    if isinstance(on, str):
+        if on not in df_columns:
+            raise ValueError("{} not a column in df".format(on))
+    elif isinstance(on, list):
+        for col in on:
+            if col not in df_columns:
+                raise ValueError("{} not a column in df".format(col))
+
+
+def _check_featuredata(data, on, **kwargs):
+    """
+    Check feature data is numerical
+    """
+    feature_cols = _get_featuredata(data, **kwargs)
+    cols_to_check = [col for col in feature_cols if col not in [on]]
+    df_to_check = data[cols_to_check]
+    is_number = np.vectorize(lambda x: np.issubdtype(x, np.number))
+    if any(is_number(df_to_check.dtypes) == False):
+        raise ValueError("non-numeric column found in feature data")
+
+
+def _get_featuredata(data, metadata_string="Metadata", prefix=True):
+    """
+    identifies columns in a dataframe that are not labelled with the
+    metadata prefix. Its assumed everything not labelled metadata is
+    featuredata
+
+    Parameters
+    ----------
+
+    data : pandas DataFrame
+        DataFrame
+
+    metadata_string : string (default="Metadata")
+        string that denotes a column is a metadata column
+
+    prefix: boolean (default=True)
+        if True, then only columns that are prefixed with metadata_string are
+        selected as metadata. If False, then any columns that contain the
+        metadata_string are selected as metadata columns
+
+    Returns
+    -------
+    f_cols : list
+        List of feature column labels
+    """
+    if prefix:
+        f_cols = [i for i in data.columns if not i.startswith(metadata_string)]
+    elif prefix is False:
+        f_cols = [i for i in data.columns if metadata_string not in i]
+    return f_cols
+
+
+def _get_metadata(data, metadata_string="Metadata", prefix=True):
+    """
+    identifies column in a dataframe that are labelled with the metadata_prefix
+
+    Parameters
+    ----------
+    data : pandas DataFrame
+        DataFrame
+
+    metadata_string : string (default="Metadata")
+        string that denotes a column is a metadata column
+
+    prefix: boolean (default=True)
+        if True, then only columns that are prefixed with metadata_string are
+        selected as metadata. If False, then any columns that contain the
+        metadata_string are selected as metadata columns
+
+    Returns
+    -------
+    m_cols : list
+        list of metadata column labels
+    """
+    if prefix:
+        m_cols = [i for i in data.columns if i.startswith(metadata_string)]
+    elif prefix is False:
+        m_cols = [i for i in data.columns if metadata_string in i]
+    return m_cols
