d4b443c77f275ce3874e126afb5430396329dd80
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-10-14 12:56:32 +0100

    convert tabs to spaces

--- 1_make_LoadData/create_image_list.sh
+++ 1_make_LoadData/create_image_list.sh
@@ -24,29 +24,29 @@ if [ ! -d "$1" ]
         echo " ERROR: $1 is not a valid directory"
         exit 1
     else
-	# for every subdirectory in directory parent directory
-	# find files that do not contain "thumb" or ".db"
-	for D in "$1"*;
-	do
-	    if [ -d "${D}" ]
-	    then
-		# get final platename from the filepath to prefix
-		#the $_filenames.txt
-		platename=$(echo "${D}" | sed "s/.*\///")
-		if [ ! -z "$2" ]
-		then
-		# if second argument has been passed
-		# save to the argument location
-		    find "${D}" -type f | grep -v "thumb\|.db" > \
-			"$2"/"$platename"_filenames.txt 
-		else
-		    # save to ./tmp/
-		    find "${D}" -type f | grep -v "thumb\|.db" > \
-			tmp/"$platename"_filenames.txt
-		fi
-		    echo -e "\t$platename"
-	    fi
-	done
+    # for every subdirectory in directory parent directory
+    # find files that do not contain "thumb" or ".db"
+    for D in "$1"*;
+    do
+        if [ -d "${D}" ]
+        then
+        # get final platename from the filepath to prefix
+        #the $_filenames.txt
+        platename=$(echo "${D}" | sed "s/.*\///")
+        if [ ! -z "$2" ]
+        then
+        # if second argument has been passed
+        # save to the argument location
+            find "${D}" -type f | grep -v "thumb\|.db" > \
+            "$2"/"$platename"_filenames.txt 
+        else
+            # save to ./tmp/
+            find "${D}" -type f | grep -v "thumb\|.db" > \
+            tmp/"$platename"_filenames.txt
+        fi
+            echo -e "\t$platename"
+        fi
+    done
 fi
 
 
