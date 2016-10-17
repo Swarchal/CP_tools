79d4dc5d3e43d6876e6aa9ae0435793532649a51
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-10-17 14:52:42 +0100

    shorten the names

---
+++ 1_make_LoadData/load_data.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# takes a directory path of file-lists:
+#   - parses metadata
+#   - reshapes
+
+# arguments
+# ----------
+# first argument: directory name containing file-lists
+# second argument (optional) output location
+
+# should only read in files with .filelist extension
+# as we may have other files in the same directory
+
+# check user has passed at least one argument
+if [ $# -eq 0 ]
+then
+    echo "ERROR: no arguments supplied"
+    exit 1
+fi
+
+
+# capture directory from stdin
+while read line
+do
+    var="${line}"
+done < /dev/stdin
+
+
+if [ ! -d $var ]
+then
+    echo "ERROR $var is not a valid directory"
+    exit 1
+else
+    # loop through image-list files and call create_loadData on each
+    for D in "$var"*.filelist
+    do
+        # get platename between last slash and dot
+        platename_with_file=${D%.*}
+        platename=${platename_with_file##*/}
+        # extract metadata from image headings
+        python src/indv/create_loadData.py "${D}" "$1"/"$platename".load_data.csv
+    done
+    echo "$1"
+fi
+
+exit 0
---
+++ 1_make_LoadData/reshape.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# take directory location from stdin
+
+# with directory being containing the load_data_inputs created from
+# create_load_data_input.sh
+
+# argument is the output location of the reshaped
+
+# check user has passed at least one argument
+if [ $# -eq 0 ]
+then
+    echo "ERROR: no arguments supplied"
+    exit 1
+fi
+
+
+# read input from stdin and set to a variable
+while read line
+do
+    var="${line}"
+done < /dev/stdin
+
+
+# check if we have a valid directory
+if [ ! -d "$var" ]
+then
+    echo "ERROR: $var is not a valid directory"
+    exit 1
+else
+    # for csv file in the directory
+    for D in "$var"*".load_data.csv"
+    do
+        # get the platename between last slash and dot
+        platename_with_ext=${D%.*}
+        platename=${platename_with_ext##*/}
+        Rscript src/indv/reshape.R "${D}" "$1"/"$platename".csv
+    done
+fi
+
+exit 0
