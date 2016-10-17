6535d8cc2ad3d5d0c75793ff1ef5021e08cef4f7
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-10-17 17:59:05 +0100

    command-line batch_list creation

---
+++ 2_batch_LoadData/batch_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import argparse
+import create_batch_list
+
+parser = argparse.ArgumentParser(description="create cellprofiler batch commands")
+parser.add_argument("-f", "--file_list", required=True, help="path to .csv file for LoadData")
+parser.add_argument("-p", "--pipeline", required=True, help="cellprofiler pipeline")
+parser.add_argument("-c", "--n_chunks")
+parser.add_argument("-s", "--chunk_size")
+parser.add_argument("-o", "--prefix")
+parser.add_argument("-q", "--full_path")
+
+parser.set_defaults(chunk_size=int(20),
+                    prefix="output",
+                    full_path=True)
+
+args = parser.parse_args()
+
+out = create_batch_list.create_batch_list(file_list=str(args.file_list),
+                                          pipeline=str(args.pipeline),
+                                          chunk_size=int(args.chunk_size),
+                                          output_prefix=str(args.prefix),
+                                          full_path=bool(args.full_path))
+
+print(*out, sep="\n")
