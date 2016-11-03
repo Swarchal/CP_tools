35dcbca7c394ec72f4b25d72c4ca56b1970dd179
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-11-03 16:37:12 +0000

    first working version of main!

--- main.sh
+++ main.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#-------------------------------------------------------------------------------
+# Abandon all hope ye who enter here...
+#
+# This is a convoluted mess of bash/sed/grep, python and R, all using a mix of
+# command line args and stdin.
+#
+# I have no idea of how it all works and it's probably developed some sort of
+# sentience, so be absolutely sure before changing anything as there are zero
+# tests.
+#------------------------------------------------------------------------------
 
 ###############################################################################
 # automatically generate cellprofiler submission scripts to be run on SGE
@@ -38,33 +48,51 @@ done
 if [ ! -d file_lists ]
 then
     mkdir file_lists
+    if [ ! -d file_lists ]
+    then
+        echo "ERROR: failed to create file_lists directory"
+        exit 1
+    fi
 fi
+
 
 if [ ! -d batch_commands ]
 then
     mkdir batch_commands
+    if [ ! -d batch_commands ]
+    then
+        echo "ERROR: failed to create batch_commands directory"
+        exit 1
+    fi
 fi
+
 
 if [ ! -d submission_scripts ]
 then
     mkdir submission_scripts
+    if [ ! -d submission_scripts ]
+    then
+        echo "ERROR failed to create submission scripts directory"
+    fi
 fi
+
 
 ###############################################################################
 # 1 CREATE FILE LISTS
 ###############################################################################
 
 # get full path of file_lists directory
-cd file_list
-file_lists_loc="${pwd}"
+cd file_lists
+file_lists_loc="$(pwd)"
 
-cd 1_make_LoadData
+cd ../1_make_LoadData
 
-./create_load_data.sh "$IMG_LOC" "${file_lists_loc}"
+./create_load_data.sh "$IMG_LOC" "$file_lists_loc"
 
 # create list of file_lists
 cd ../file_lists
 ls > ../.file-list.txt
+
 
 ###############################################################################
 # 2 CREATE LOADDATA FILES
@@ -74,7 +102,9 @@ cd ../2_batch_LoadData
 
 while read f
 do
-    ./batch_list.py -f "$tmp_loc"/"$f" -p "$PIPELINE_LOC" > ../batch_commands/"$f"
+    ./batch_list.py --file_list "$file_lists_loc"/"$f" \
+        --pipeline "$PIPELINE_LOC" \
+        --path_prefix "$SCRATCH" > ../batch_commands/"$f"
 done < ../.file-list.txt
 
 
@@ -85,7 +115,7 @@ done < ../.file-list.txt
 
 cd ../batch_commands
 # get path location of batch_files
-batch_loc=${pwd}
+batch_loc=$(pwd)
 # get list of batch command files and store as a text file for loop
 ls > ../.batch_list.txt
 
@@ -96,9 +126,9 @@ cd ../3_batch_insert
 # (templace_job.sh)
 while read f
 do
-    ./batch_script.py -f "$batch_loc"/"$f" \
-        -t template_job.sh
-        -o ../submission_scripts/
+    ./batch_script.py --batch_file "$batch_loc"/"$f" \
+        --template template_job.sh \
+        --output_location ../submission_scripts/
 done < ../.batch_list.txt
 
 
