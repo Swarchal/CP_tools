20e87de62160bc2ab99c2dfaf719ad5fba19299d
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-11-21 17:22:41 +0000

    python port of create_image_list framework

---
+++ 1_make_LoadData/create_image_list.py
@@ -0,0 +1,137 @@
+"""
+Class to create LoadData csv files from an ImageXpress experiment directory
+"""
+
+import json
+import pandas as pd
+# TODO sort out parse_path functions for extracting metadata from filenames
+import parse_paths as pp
+import os
+
+class ImageList(object):
+    """
+    Create image lists from an ImageXpress experiment directory
+    """
+
+    def __init__(self, exp_dir):
+        self.exp_dir = exp_dir
+        self.img_files = []
+        self.plate_names = set()
+        self.plate_img_store = dict()
+        self.load_data_files = dict()
+
+
+    def __len__(self):
+        """number of plates"""
+        return len(self.plate_names)
+
+
+    def get_img_files(self, ext="tif"):
+        """
+        Given an ImageXpress experiment directory, this function will:
+        - return a list of valid image paths for all plates in that experiment.
+        - return a set of platenames
+        """
+        # TODO : speed this up! really slow.
+        #        worse-case could call a find command in subprocess
+        for root, _, files in os.walk(self.exp_dir):
+            for f in files:
+                if f.endswith(ext) and "thumb" not in f:
+                    full_path = os.path.join(root, f)
+                    self.img_files.append(full_path)
+                    platename = full_path.split(os.sep)[-4]
+                    self.plate_names.add(platename)
+
+
+    def create_plate_img_store(self):
+        """
+        Create a dictionary:
+        e.g {plate_name  : [list of img files],
+             plate_name2 : [list of img files]}
+        """
+        # call get_img_files if it has not already been called
+        if len(self.plate_names) == 0 or len(self.img_files) == 0:
+            self.get_img_files()
+        for plate in self.plate_names:
+            # get only the image paths for a particular plate
+            plate_img = [s for s in self.img_files if plate in s]
+            # store in dictionary {plate : image_list}
+            self.plate_img_store[plate] = plate_img
+
+
+    def to_json(self, location):
+        """
+        store dictionary as JSON file
+
+        Parameters:
+        ------------
+        location (string) :
+            file path to save JSON file
+        """
+        with open(location, "wb") as save_point:
+            json.dump(self.plate_img_store, save_point, indent=4)
+
+
+    def from_json(self, location):
+        """
+        create plate : image_list dictionary from JSON dump
+
+        Parameters:
+        ------------
+        location (string):
+            file path of JSON file
+        """
+        with open(location, "r") as load_point:
+            self.plate_img_store = json.load(load_point)
+
+
+    def parse_metadata(self):
+        """
+        parse metadata from file paths and convert into a dataframe
+        """
+        col_names = ["URL", "path", "Metadata_platename", "Metadata_well",
+                     "Metadata_site", "Metadata_channel", "Metadata_platenum"]
+        if len(self.plate_img_store) == 0:
+            raise ValueError("plate_img_store is empty")
+        # loop through dictionary of file-paths
+        for plate, file_list in self.plate_img_store.items():
+            filenames = pp.get_filename(file_list)
+            pp.check_filename(filenames)
+            df = pd.DataFrame(
+                zip(
+                    filenames,
+                    pp.get_path(file_list),
+                    pp.get_platename(file_list),
+                    pp.get_metadata_well(file_list),
+                    pp.get_metadata_site(file_list),
+                    pp.get_metadata_channel(file_list),
+                    pp.get_platenum(file_list)
+                    )
+            )
+            df.columns = col_names
+            # reshape from long to wide format
+            wide_df = self._cast_dataframe(df)
+            self.load_data_files[plate] = wide_df
+
+
+    def _cast_dataframe(self, dataframe):
+        """
+        reshape load_data_files dataframes from long to wide format
+        """
+        n_channels = len(set(dataframe.Metadata_channel))
+        wide_df = dataframe.pivot_table(
+            index=["Metadata_site", "Metadata_well", "Metadata_platenum",
+                   "Metadata_platename", "path"],
+            columns="Metadata_channel",
+            values="URL",
+            aggfunc="first").reset_index()
+        # rename FileName columns from 1, 2... to FileName_W1, FileName_W2 ...
+        columns = dict()
+        for i in range(1, n_channels+1):
+            columns[i] = "FileName_W" + str(i)
+        wide_df.rename(columns=columns, inplace=True)
+        # duplicate PathName for each channel
+        for i in range(1, n_channels+1):
+            wide_df["PathName_W" + str(i)] = wide_df.path
+        wide_df.drop(["path"], axis=1, inplace=True)
+        return wide_df
---
+++ 1_make_LoadData/parse_paths.py
@@ -0,0 +1,92 @@
+import os
+
+
+def get_filename(file_list):
+    """return the final image URLS from a file path"""
+    files = []
+    for line in file_list:
+        filename = str(line.split(os.sep)[-1])
+        files.append(filename.strip())
+    return files
+
+
+def check_filename(file_list, char="_"):
+    """sanity check list of filenames"""
+    for f in file_list:
+        if len(f) < 20:
+            raise ValueError("Filename {} too short".format(f))
+        if len(f.split(char)) < 4:
+            raise ValueError("Filename {} contains too few metadata values".format(f))
+
+
+def get_path(file_list):
+    """return the path up until the image URL"""
+    paths = []
+    for line in file_list:
+        p, f = os.path.split(line)
+        paths.append(p.strip())
+    return paths
+
+
+def get_platename(file_list):
+    """return platename from file path"""
+    plates = []
+    for line in file_list:
+        plates.append(line.split(os.sep)[-4])
+    return plates
+
+
+def get_platenum(file_list):
+    """return plate number"""
+    plates = []
+    for line in file_list:
+        plates.append(line.split(os.sep)[-2])
+    return plates
+
+
+def get_date(file_list):
+    """return date of imaging for a plate"""
+    dates = []
+    for line in file_list:
+        dates.append(line.split(os.sep)[-3])
+    return dates
+
+
+def split_filename(file_list, char="_"):
+    """split filename by characher"""
+    url = get_filename(file_list)
+    filename = []
+    for line in url:
+        filename.append(line.split(char))
+    return filename
+
+
+def get_metadata_well(file_list, char="_"):
+    """return well labels from file paths"""
+    url = get_filename(file_list)
+    wells = []
+    for line in url:
+        wells.append(str(line.split(char)[1]))
+    return wells
+
+
+def get_metadata_site(file_list, char="_"):
+    """return imaging sites/field-of-view from filepaths"""
+    url = get_filename(file_list)
+    sites = []
+    for line in url:
+        site_str = line.split(char)[2]
+        site_num = int("".join(x for x in site_str if x.isdigit()))
+        sites.append(site_num)
+    return sites
+
+
+def get_metadata_channel(file_list, char="_"):
+    """return channel numbers from file paths"""
+    url = get_filename(file_list)
+    channels = []
+    for line in url:
+        ch = int(line.split(char)[3][1])
+        channels.append(ch)
+    return channels
+
