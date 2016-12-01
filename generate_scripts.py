5e7a4488513d400ce89d6e53374b7b40bdb0cc4f
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-12-01 16:33:20 +0000

    improve help messages

--- generate_scripts.py
+++ generate_scripts.py
@@ -7,19 +7,24 @@ from create_image_list import ImageList
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-e", "--experiment", type=str,
-                    help="experiment directory")
+                    help="experiment directory. The top level file in the\
+ImageExpress folder that contains plates as sub-directories.")
 
 parser.add_argument("-l", "--loaddata-location", type=str,
-                    help="location to store LoadData files")
+                    help="location to store LoadData files. These locations\
+will be used in the SGE submission scripts for the LoadData module, so this\
+location should have good IO performance")
 
 parser.add_argument("-p", "--pipeline", type=str,
-                    help="path to cellprofiler pipeline")
+                    help="path to cellprofiler pipeline.")
 
 parser.add_argument("-r", "--path-prefix", type=str,
-                    help="prefix for results location")
+                    help="prefix for results location. This is where the output\
+will be saved. The user's scratch space is recommended")
 
 parser.add_argument("-o", "--script-location", type=str,
-                    help="location to save submission scripts")
+                    help="location to save submission scripts. This is where\
+SGE submission scripts will be saved to.")
 
 arguments = parser.parse_args()
 
