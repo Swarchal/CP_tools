0d250e0316f382ad7cfdadd66737949b58aefa18
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-12-01 16:41:55 +0000

    update to new virtualenv location

--- template_job.sh
+++ template_job.sh
@@ -2,18 +2,16 @@
 #$ -l h_vmem=12G
 #$ -l h_rt=12:00:00
 #$ -j y
-#$ -o /exports/eddie/scratch/cp.log
+#$ -o /exports/eddie/scratch/s1027820/cp.log
 
 . /etc/profile.d/modules.sh
 module load igmm/apps/hdf5/1.8.16
 module load igmm/apps/python/2.7.10
 module load igmm/apps/jdk/1.8.0_66
 module load igmm/libs/libpng/1.8.18
-source ~/virtualenv-1.10/myVE/bin/activate
-source ~/.bash_profile
 
-# directory containing images list and pipeline
-# or could use absolute paths in the pipeline commands
+source /exports/igmm/eddie/Drug-Discovery/virtualenv-1.10/myVE/bin/activate
+source ~/.bash_profile
 
 # placeholder string to be substituted with cellprofiler command
 PLACEHOLDER
