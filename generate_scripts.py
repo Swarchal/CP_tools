d9faa10590067575f413e054dde6bd11220a2f4e
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-12-01 16:38:08 +0000

    expand help information

--- generate_scripts.py
+++ generate_scripts.py
@@ -7,24 +7,27 @@ from create_image_list import ImageList
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-e", "--experiment", type=str,
-                    help="experiment directory. The top level file in the\
+                    help="Experiment directory. The top level directory in the \
 ImageExpress folder that contains plates as sub-directories.")
 
 parser.add_argument("-l", "--loaddata-location", type=str,
-                    help="location to store LoadData files. These locations\
-will be used in the SGE submission scripts for the LoadData module, so this\
+                    help="Location to store LoadData files. These locations \
+will be used in the SGE submission scripts for the LoadData module, so this \
 location should have good IO performance")
 
 parser.add_argument("-p", "--pipeline", type=str,
-                    help="path to cellprofiler pipeline.")
+                    help="Path to cellprofiler pipeline.")
 
 parser.add_argument("-r", "--path-prefix", type=str,
-                    help="prefix for results location. This is where the output\
-will be saved. The user's scratch space is recommended")
+                    help="Prefix for results location. This is where the output \
+will be saved. This needs to be somewhere with large amounts of space and good \
+write performance - the user's scratch space is recommended")
 
 parser.add_argument("-o", "--script-location", type=str,
-                    help="location to save submission scripts. This is where\
-SGE submission scripts will be saved to.")
+                    help="Location to save submission scripts. This is where \
+SGE submission scripts will be saved to, this location is not critical if full \
+paths are used for the other arguments, although for large jobs these can take \
+up several Gigabytes of space.")
 
 arguments = parser.parse_args()
 
